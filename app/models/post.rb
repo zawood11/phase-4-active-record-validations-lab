76835cb56d4c34a1e4a84f0483b501f9efb8ccd5
Zach Underwood <zawood11@gmail.com>
2022-04-23 20:00:56 -0400

    completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+        titles = [/Won't Believe/i,/Secret/i,/Top \d/i,/Guess/i] 
+        if titles.none? {|phrase| phrase.match title } 
+            errors.add(:title, "Not a valid title")
+        end
+    end
 end
